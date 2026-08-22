53a9a80bf69ff4e44321d6dd3112fb0352b5efb3
C. Krichenbauer <schule@krichenbauer.de>
2026-08-22 09:35:14 +0200

    inital commit: flake

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Visual OpenSCAD development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          nodejs_24
+          pnpm
+          git
+        ];
+      };
+    };
+}
